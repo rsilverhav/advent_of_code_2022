54634c84c48f9946381dfcacc385838182f8e5be
Robin Silverhav <robinsilverhav@gmail.com>
2023-01-03 11:06:36 +0100

    refactored 3a

--- src/3/parseRucksack.ts
+++ src/3/parseRucksack.ts
@@ -1,12 +1,20 @@
 export function parseRucksack(input: string) {
-  if (input.length % 2 !== 0) {
-    throw new Error('Uneaven input length')
-  }
+  return parseRucksackCollections(() => {
+    if (input.length % 2 !== 0) {
+      throw new Error('Uneaven input length')
+    }
 
-  const part1 = input.substring(0, input.length / 2).split('')
-  const part2 = input.substring(input.length / 2, input.length).split('')
+    const part1 = input.substring(0, input.length / 2)
+    const part2 = input.substring(input.length / 2, input.length)
 
-  const commonItem = part1.find((p1) => part2.find((p2) => p1 === p2))
+    return [part1, part2]
+  })
+}
+
+function parseRucksackCollections(getItemCollection: () => string[]) {
+  const collections = getItemCollection()
+
+  const commonItem = getCommonItem(collections)
 
   if (typeof commonItem === 'undefined') {
     throw new Error('No common item in compartments')
@@ -14,4 +22,22 @@ export function parseRucksack(input: string) {
 
   const value = commonItem.charCodeAt(0) - (/[A-Z]/.test(commonItem) ? 38 : 96)
   return value
+}
+
+function getCommonItem(collections: string[]): string | undefined {
+  const allUniqueItems = new Set<string>(
+    collections.reduce((acc, next) => acc + next, '').split('')
+  )
+
+  for (const [item] of allUniqueItems.entries()) {
+    let collectionsWithItem = collections.reduce(
+      (sum, next) => sum + (next.includes(item) ? 1 : 0),
+      0
+    )
+    if (collectionsWithItem === collections.length) {
+      return item
+    }
+  }
+
+  return undefined
 }
