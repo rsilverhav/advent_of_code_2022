c7e5fbdf9c7c425daebab9c530051472d110a0b9
Robin Silverhav <robinsilverhav@gmail.com>
2023-01-08 16:26:00 +0100

    refactored 2

--- src/2/parseRPS.ts
+++ src/2/parseRPS.ts
@@ -1,20 +1,20 @@
 export function parseRPSConstant(input: string) {
-  return parseRPS(input, (playerChar: string) => convertPlayerInput(playerChar))
+  return parseRPS(input, (playerChar: string) => convertPlayerInput(playerChar));
 }
 
 export function parseRPSReactive(input: string) {
   return parseRPS(input, (playerChar: string, opponentInput: RPSInput) => {
     switch (playerChar) {
       case 'X':
-        return getLossInput(opponentInput)
+        return getLossInput(opponentInput);
       case 'Y':
-        return getDrawInput(opponentInput)
+        return getDrawInput(opponentInput);
       case 'Z':
-        return getWinInput(opponentInput)
+        return getWinInput(opponentInput);
       default:
-        throw new Error(`Unknown input char ${playerChar}`)
+        throw new Error(`Unknown input char ${playerChar}`);
     }
-  })
+  });
 }
 
 enum RPSInput {
@@ -23,40 +23,52 @@ enum RPSInput {
   Scissor = 3,
 }
 
+const PlayerMoveMap: Record<string, RPSInput> = {
+  X: RPSInput.Rock,
+  Y: RPSInput.Paper,
+  Z: RPSInput.Scissor,
+};
+
+const OpponentMoveMap: Record<string, RPSInput> = {
+  A: RPSInput.Rock,
+  B: RPSInput.Paper,
+  C: RPSInput.Scissor,
+};
+
 function convertPlayerInput(playerChar: string): RPSInput {
-  return playerChar.charCodeAt(0) - 87
+  return PlayerMoveMap[playerChar];
 }
 
 function parseRPS(
   input: string,
   getPlayerInput: (playerChar: string, opponentInput: RPSInput) => RPSInput
 ) {
-  const [opponentChar, playerChar] = input.split(' ')
+  const [opponentChar, playerChar] = input.split(' ');
 
-  const opponentInput = convertOpponentInput(opponentChar)
-  const playerInput = getPlayerInput(playerChar, opponentInput)
+  const opponentInput = convertOpponentInput(opponentChar);
+  const playerInput = getPlayerInput(playerChar, opponentInput);
 
   if (opponentInput === playerInput) {
-    return playerInput + 3
+    return playerInput + 3;
   } else if (opponentInput === getLossInput(playerInput)) {
-    return playerInput + 6
+    return playerInput + 6;
   } else {
-    return playerInput
+    return playerInput;
   }
 }
 
 function convertOpponentInput(opponentChar: string): RPSInput {
-  return opponentChar.charCodeAt(0) - 64
+  return OpponentMoveMap[opponentChar];
 }
 
 function getLossInput(originalInput: RPSInput) {
-  return originalInput === RPSInput.Rock ? RPSInput.Scissor : originalInput - 1
+  return originalInput === RPSInput.Rock ? RPSInput.Scissor : originalInput - 1;
 }
 
 function getWinInput(originalInput: RPSInput) {
-  return originalInput === RPSInput.Scissor ? RPSInput.Rock : originalInput + 1
+  return originalInput === RPSInput.Scissor ? RPSInput.Rock : originalInput + 1;
 }
 
 function getDrawInput(originalInput: RPSInput) {
-  return originalInput
+  return originalInput;
 }
